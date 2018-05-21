7d7edc25e8a9ff6f449dcf87bdadf290bbe63804
mbh038 <mbh038@gmail.com>
2018-05-21 08:11:32 +0100

    census

--- tutorial/app.R
+++ tutorial/app.R
@@ -2,18 +2,36 @@ library(shiny)
 
 # Define UI ----
 ui <- fluidPage(
-  
-  titlePanel("title panel"),
-  
-  sidebarLayout(position="right",
-    sidebarPanel("sidebar panel"),
-    mainPanel("main panel")
+    fluidRow(
+      column(4,
+             
+             # Copy the line below to make a slider bar 
+             sliderInput("slider1", label = h3("Slider"), min = 0, 
+                         max = 100, value = 50)
+      ),
+      column(4,
+             
+             # Copy the line below to make a slider range 
+             sliderInput("slider2", label = h3("Slider Range"), min = 0, 
+                         max = 100, value = c(40, 60))
+      )
+    ),
+    
+    hr(),
+    
+    fluidRow(
+      column(4, verbatimTextOutput("value")),
+      column(4, verbatimTextOutput("range"))
+    )
+    
   )
-  
-)
+
 
 # Define server logic ----
 server <- function(input, output) {
+  
+  output$value <- renderPrint({ input$slider1 })
+  output$range <- renderPrint({ input$slider2 })
   
 }
 
